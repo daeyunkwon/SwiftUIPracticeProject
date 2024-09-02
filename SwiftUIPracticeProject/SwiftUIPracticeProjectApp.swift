50e7f4ed3f8d2ef93151d2902b07d45f4a8d3118
Daeyun <sweet_ray@naver.com>
2024-09-02 17:31:31 +0900

    Initial Commit

---
+++ SwiftUIPracticeProject/SwiftUIPracticeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeProjectApp.swift
+//  SwiftUIPracticeProject
+//
+//  Created by 권대윤 on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
